9fd78e342e47c80800dc9879bd4dc3ee53ff6a41
mjmmkn <mjmmkn@gmail.com>
2019-11-28 10:09:16 +0900

    非同期通信実装（if文修正）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function(){
   function buildHTML(message){
-    // console.log(message)
-    if (message.image) {
+    image = (message.image) ? `<img class "lower-message" src=${message.image} >`:'';
       let html = `<div class="message">
       <div class="upper-message">
         <div class="upper-message__user-name">
@@ -11,40 +10,19 @@ $(function(){
           ${message.created_at}
         </div>
       </div>
-      <div class="lower-message">
-      ${ message.image}
+      <div class= "lower-message">
+        ${image}
           <p class="lower-message__content">
             ${ message.content }
           </p>
       </div>
     </div>`
     return html;
-    
-    } else {
-      let html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${ message.user_name }
-                      </div>
-                      <div class="upper-message__date">
-                        ${ message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                    <p class="lower-message__content">
-                      ${ message.content}
-                    </p>
-                    </div>
-                  </div>`
-       return html;
     }
-      
-  }
+
 
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
-    // console.log('a')
-    // let form = $(".form__message").val();
     let formData = new FormData(this);
     let url = $(this).attr('action')
     $.ajax({
@@ -59,7 +37,7 @@ $(function(){
       let html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('.form__message').val('');
+      $('.new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
       })
     .fail(function() {
