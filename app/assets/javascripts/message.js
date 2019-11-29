48b08aa81873af352bea1bc70e69e449ba2dcf21
mjmmkn <mjmmkn@gmail.com>
2019-11-29 10:01:21 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,8 @@
 $(function(){
   function buildHTML(message){
-    image = (message.image) ? `<img class "lower-message" src=${message.image} >`:'';
-      let html = `<div class="message">
+    let content = (message.content) ? `${ message.content}`:"";
+    let image = (message.image) ? `<img class "lower-message" src=${message.image} >`:'';
+      let html = `<div class="message" data-message-id="${message.id}">
       <div class="upper-message">
         <div class="upper-message__user-name">
           ${ message.user_name }
@@ -13,7 +14,7 @@ $(function(){
       <div class= "lower-message">
         ${image}
           <p class="lower-message__content">
-            ${ message.content }
+            ${content}
           </p>
       </div>
     </div>`
@@ -44,5 +45,32 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
 
-    });
+  });
+  let reloadMessages = function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    let href = 'api/messages#index {:format=>"json"}'
+    let last_message_id = $('.message:last').data('message-id');
+
+    $.ajax({
+      url:  href,
+      type: 'GET',
+      data: {id: last_message_id},
+      dataType: 'json'
+    })
+
+
+    .done(function(messages){
+      let insertHTML='';
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        });
+    })
+        .fail(function() {
+          alert("自動更新に失敗しました");
+        });
+      }
+      };
+      setInterval(reloadMessages, 7000);
   });
